234135fbb7744c58161a93765f616a6ec2205e9a
Luis Bustos <luisbustos500@gmail.com>
2022-12-09 09:12:42 -0700

    basic functionality

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let arrWords = [
 'quiz',
 'executioner',
 'nauseous',
-'Rhythm',
+'rhythm',
 ];
 
 //function to pick random word from array
@@ -21,34 +21,75 @@ const randomWord = () => {
 randomWord();
 console.log(gameWord);
 
+
 //function that takes random word and splits it / display amount of words 
-
+let guessed = [];
+let wordSplit;
 const wordToGuess = () => {
-    //gameWord.split('')
-    document.getElementById('wordDisplay') 
+    wordSplit = gameWord.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : ' _ ')).join('');
     
+   document.getElementById('wordDisplay').innerHTML = wordSplit;
 }
-wordToGuess();
+wordToGuess(); 
 
 
-//get answer from letter buttons  
+//generate letter buttons  
+const generateButtons = () => {
+    let abcButtons = 'abcdefghijklmnopqrstuvwxyz'.split('').map(i =>
+        `<button id='${i}' onClick='makeGuess("${i}")'>${i}</button>`).join(' ');
+
+        document.getElementById('abcd').innerHTML = abcButtons 
+}
+generateButtons();
 
 
+//button onclick to make guesses
+const makeGuess = (letterGuessed) => {
+    if(guessed.indexOf(letterGuessed) === -1){
+        guessed.push(letterGuessed)} 
+
+    document.getElementById(letterGuessed).setAttribute('disabled', true);
+
+    if(gameWord.indexOf(letterGuessed) >= 0){
+        wordToGuess();
+        // should check if won game
+    } else if(gameWord.indexOf(letterGuessed) === -1){
+        updateWrongGuess();
+        checkIfGameLost();
+    }
+}
+
+//update wrong guess
+let wrongGuess = 0;
+const updateWrongGuess = () => {
+    wrongGuess++
+document.getElementById('wrongG').innerHTML = wrongGuess
+}
 
 
-//player 1 
-//number of correct guesses and missed guesses 
+//if wrong guess exceeds max wrong guess / lost game
+const checkIfGameLost = () => {
+    if(wrongGuess >= 5){
+        //update score
+        nextRound() // proceeds with next round
+    } 
+}
 
 
+//if correctly guesses word / win
+const ifGameWon = () => {
 
-//player 2 
-//number of correct guesses and missed guesses 
-
-
-
+}
 
 
-//function to starts next round for second player / update score
+//update score for players
+const updateScore = () => {
 
+}
+
+//function to starts next round for second player 
+const nextRound = () => {
+
+}
 
 //reset game
